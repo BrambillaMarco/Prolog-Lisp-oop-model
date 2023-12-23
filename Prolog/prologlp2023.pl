c40f043abbeba1e2ab2bbd426523be743877c21b
Paolo Condello <p.condello@campus.unimib.it>
2023-12-23 09:54:44 +0100

    is_instance/2 e inst/2
    Ridefinita is_instance, ora funzionante ed in linea con quanto scritto sulla traccia.
    
    Ridefinita inst, ancora da testare, dovrebbe andare ma a quanto pare entra in una specie di loop infinito.
    
    Rimosso il vecchio predicato legacy che è stato sostituito da check_legacy.

--- Prolog/prologlp2023.pl
+++ Prolog/prologlp2023.pl
@@ -6,6 +6,7 @@
 def_class(ClassName, Parents):-
     def_class(ClassName, Parents, []).
 
+
 % def_class/3
 % definisce una classe con nome, genitori, campi e metodi
 def_class(ClassName, Parents, Parts):-
@@ -17,6 +18,7 @@ def_class(ClassName, Parents, Parts):-
     append(InheritedParts, Parts, AllParts),
     assert(class(ClassName, Parents, AllParts)).
 
+
 % check_parents/1
 % verifica se esistono le classi genitori
 check_parents([]).
@@ -24,18 +26,6 @@ check_parents([Parent | Parents]):-
     class(Parent, _, _),
     check_parents(Parents).
 
-
-% legacy(_, [], [], []).
-
-% legacy(ClassName, [], PastParents, In):-
-%    retract(class(ClassName, _, _)),
-%    assert(class(ClassName, PastParents, In)).
-
-% legacy(ClassName, [Parent|Parents], PastParents, In):-
-%    findall(Parts, class(Parent, _, Parts), FA),
-%    append(In, FA, Out),
-%    append([Parent], PastParents, PP),
-%    legacy(ClassName, Parents, PP, Out).
 
 % check_parts/1
 % verifica se campi e/o metodi sono scritti correttamente
@@ -49,6 +39,7 @@ check_parts([Part | Rest]):-
 check_parts([Part|Rest]):-
     Part=method(_, _, _),
     check_parts(Rest).
+
 
 % check_legacy/2
 % eredita i campi e/o i metodi delle classi genitore
@@ -70,38 +61,34 @@ make(InstanceName, ClassName):-
     var(InstanceName),
     InstanceName=instance(InstanceName, ClassName, []).
 
+
 % is_class/1
 % verifica se esiste la classe
 is_class(ClassName) :-
     class(ClassName,_,_).
+
 
 % is_instance/1
 % verifica se esiste un'istanza generica
 is_instance(Value) :-
     instance(Value, _, _).
 
+
 % is_instance/2
-% verifica se esiste un'istanza con una determinata
-% superclasse
-is_instance(Value, ClassName) :-
-    check_superclass(ClassName, Class),
-    instance(Value, Class, _).
+% verifica se esiste un'istanza con una determinato
+% genitore/superclasse
+is_instance(Value, SuperClass) :-
+    instance(Value, Class, _),
+    class(Class, Parents, _),
+    member(SuperClass, Parents).
 
-
-% check_superclass/2
-% verifica se una classe è figlia di un'altra classe
-check_superclass([], _).
-check_superclass(SuperClass, Class) :-
-    class(Class, [SuperClass | OtherSuperClass], _),
-    check_superclass(OtherSuperClass, Class).
 
 % inst/2
 % recupera il nome con cui è stata creata una
 % istanza
 inst(InstanceName, Instance) :-
-    instance(InstanceName, _, _).
-
-
-
-
-
+    instance(InstanceName, ClassName, Fields),
+    append([Instance,
+           "e': ",
+           instance(InstanceName, ClassName, Fields)], WO),
+    write(WO).
